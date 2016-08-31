8108970393f4445a07ca2d474557658c10315cca
¨Mario <mario.vizcainos@gmail.com>
2016-08-30 22:00:56 -0500

    added notes validation and notes model test

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Note, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  note = Note.new(data: "hello")
+
+  it "is valid with valid attributes" do
+    expect(note).to be_valid
+  end
+
+  it "is not valid without a data" do
+    note.data = nil
+    expect(note).to_not be_valid
+  end
 end
