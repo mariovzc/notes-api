9ef18c802f68327035c4c342df8be9d7016c6f00
¨Mario <mario.vizcainos@gmail.com>
2016-08-30 22:24:30 -0500

    added title to the note

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 1) do
+ActiveRecord::Schema.define(version: 2) do
 
   create_table "notes", force: :cascade do |t|
     t.text     "data"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "title"
   end
 
 end
