2371c0905d6ebd34546e908189aea512568f39cf
¨Mario <mario.vizcainos@gmail.com>
2016-08-30 22:26:11 -0500

    fixed model testing

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -1,14 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Note, type: :model do
-  note = Note.new(data: "hello")
+  note = Note.new(data: "hello", title: "hi")
 
   it "is valid with valid attributes" do
     expect(note).to be_valid
   end
 
+  it "is not valid without a title" do
+    note.title = nil
+    expect(note).to_not be_valid
+  end
   it "is not valid without a data" do
     note.data = nil
     expect(note).to_not be_valid
   end
+
 end
