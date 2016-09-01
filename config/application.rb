da3583f9778cd80b5085b7a133eb6d32dec03ac0
Mario Vizcaino <mario.vizcainos@gmail.com>
2016-09-01 13:28:49 +0000

    cors test

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,21 @@ module Notes
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
+    
     config.api_only = true
+    config.middleware.insert_before 0, "Rack::Cors" do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
+    
+    use Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: :any
+      end  
+    end
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
